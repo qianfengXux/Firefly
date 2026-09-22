c92a4d90344c5cfa4a9c6cb8002fe9c6c037525f
xqf <3267139049@qq.com>
2026-09-22 20:13:41 +0800

    chore: 更新博客站点域名

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -48,7 +48,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "blog",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://xqfblog.dpdns.org/",
 
 	// 站点描述
 	description:
